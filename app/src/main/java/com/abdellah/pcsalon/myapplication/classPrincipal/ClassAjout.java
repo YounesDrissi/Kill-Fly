d474977a49850a26ce5e8c29c977505975ffc669
Younes Drissi <d.younes.2@gmail.com>
2016-03-28 00:01:31 +0200

    Bluetooth

--- app/src/main/java/com/abdellah/pcsalon/myapplication/classPrincipal/ClassAjout.java
+++ app/src/main/java/com/abdellah/pcsalon/myapplication/classPrincipal/ClassAjout.java
@@ -15,6 +15,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ExpandableListView;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.abdellah.pcsalon.myapplication.ListesSSP.Groupe;
 import com.abdellah.pcsalon.myapplication.ListesSSP.MyExpandableListAdapter;
@@ -30,6 +38,7 @@ import java.util.List;
  */
 public class ClassAjout extends AppCompatActivity {
 
+    BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
     private SparseArray<Groupe> groups = new SparseArray<Groupe>();
     private static Boolean longClick=false;
@@ -50,6 +59,7 @@ public class ClassAjout extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
 
         /*// Elements de spinner
         spinnerSite = (Spinner) findViewById(R.id.spinnerSite);
@@ -115,6 +125,53 @@ public class ClassAjout extends AppCompatActivity {
         spinnerSerie.setOnItemSelectedListener(new SerieClass());
         spinnerSite.setOnItemSelectedListener(new SiteClass());
     }*/
+
+    @Override
+    protected void onStart() {
+        Log.i("", "onStart");
+        if (bluetoothAdapter == null) {
+            Toast.makeText(ClassAjout.this,
+                    "Pas de Bluetooth", Toast.LENGTH_SHORT).show();
+            Log.e("ERROR", "No Bluetooth devices");
+        } else {
+            Toast.makeText(ClassAjout.this, "Avec Bluetooth",
+                    Toast.LENGTH_SHORT).show();
+            Log.i("INFO", "Bluetooth available");
+        }
+        private final static int REQUEST_CODE_ENABLE_BLUETOOTH = 0;
+        ….;
+        if (!bluetoothAdapter.isEnabled()) {
+            Intent enableBlueTooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBlueTooth, REQUEST_CODE_ENABLE_BLUETOOTH);
+        }
+        if (!bluetoothAdapter.isEnabled()) {
+            bluetoothAdapter.enable();
+
+        }
+        super.onStart();
+    }
+
+    private final BroadcastReceiver bluetoothReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                Toast.makeText(ClassAjout.this, "New Device = " + device.getName(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        Log.i("", "onDestroy");
+        if (bluetoothAdapter.isEnabled()) {
+            bluetoothAdapter.cancelDiscovery();
+            bluetoothAdapter.disable();
+            Log.i("INFO", "Bluetooth disable");
+        }
+        super.onDestroy();
+    }
+
 
     private void ajouterSerie() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -208,7 +265,10 @@ public class ClassAjout extends AppCompatActivity {
     private View.OnClickListener clickListenerSuivant=new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-
+            Toast.makeText(ClassAjout.this, "searching...",
+                    Toast.LENGTH_SHORT).show();
+            Log.i("INFO", "searching...");
+            bluetoothAdapter.startDiscovery();
             Intent intent = new Intent(ClassAjout.this, MainActivity.class);
             startActivity(intent);
 
